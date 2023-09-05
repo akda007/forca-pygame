93d909bf7ea4304e594580fdf139d7d2b0ad4c9a
Andrey Araujo <65751084+akda007@users.noreply.github.com>
2023-09-05 15:19:44 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,65 @@
+import pygame, ui, Keys, time
+import logic.forca as f
+import logic.words as words
+from Colors import *
+
+from tkinter import *
+from tkinter import messagebox
+
+
+def update():
+    pygame.display.flip()
+
+#initialize
+pygame.init()
+pygame.display.set_caption("Jogo da Forca!")
+
+screen = pygame.display.set_mode((600, 500))
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    forca = f.Forca(words.get_random_word("words.txt"), 6)
+
+    ui_word = ui.Word(forca.word, screen)
+
+    #main loop
+    while True:
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                running = False
+                
+            if event.type == pygame.KEYUP:
+                key_code = Keys.key_handler(event.key)
+                
+                if key_code != None:
+                    if forca.guess(key_code):
+                        ui_word.draw_char(key_code)
+
+        ui.draw_forca(screen)
+        ui.draw_character(screen, forca.lives)
+        
+        clock.tick(30)
+        update()
+        
+        if forca.lives == 0:
+            
+            res = messagebox.askyesno("GAME OVER!", f"Voce perdeu...\nA palavra era {forca.word}\nJogar novamente?")
+            running = res
+            break
+            
+        
+        if forca.verify():
+            
+            res = messagebox.askyesno("Parabens!", f"Voce adivinhou a palavra: {forca.word} corretamente!\nJogar novamente!")
+            running = res
+            
+            break
+
+pygame.quit()
+
+
+
+
